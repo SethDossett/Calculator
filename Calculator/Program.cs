2c03fc949316db7dcfa6a171b48478505cf33ab1
Seth Dossett <95620779+SethDossett@users.noreply.github.com>
2023-11-06 08:31:46 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator;
+
+internal class Program
+{
+    public static Device device = new Device();
+    public static bool EndApp = false;
+
+    public static void Main(string[] args)
+    {
+        EndApp = false;
+
+        while (!EndApp)
+        {
+            device.RunCalculator();
+        }
+
+
+    }
+
+    public static double ValidateAnswer(string result)
+    {
+        result = result.Trim();
+        while (string.IsNullOrEmpty(result) || !double.TryParse(result, out _))
+        {
+            Console.WriteLine("Please enter an integer, Try Again");
+
+            result = Console.ReadLine();
+        }
+
+        return double.Parse(result.Trim());
+    }
+
+}
